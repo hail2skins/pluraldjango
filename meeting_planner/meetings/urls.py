246366171571688883d68abb58f90fa0ca820768
Art Mills <art@hamcois.com>
2024-04-07 13:27:57 -0500

    useful django forms

--- meeting_planner/meetings/urls.py
+++ meeting_planner/meetings/urls.py
@@ -4,4 +4,5 @@ from meetings import views # Import the views module from the meetings app
 urlpatterns = [
     path('<int:id>', views.detail, name='detail'), # Add a URL pattern for the detail view
     path('rooms', views.rooms, name='rooms'), # Add a URL pattern for the rooms view
+    path('new', views.new, name='new'), # Add a URL pattern for the new view
 ]
--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect # Import the render, get_object_or_404, and redirect functions
+from django.forms import modelform_factory # Import the modelform_factory function allowing Django to create forms from models
 
 # Add the Meeting model
 from meetings.models import Meeting, Room
@@ -18,3 +19,16 @@ def rooms(request):
         request, template_name="meetings/rooms.html",
         context={"rooms": Room.objects.all()},
     )
+
+# Add a new view allowing addition of meetings
+MeetingForm = modelform_factory(Meeting, exclude=[]) # Create a form from the Meeting model
+def new(request):
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid(): # Check if the form is valid of each field
+            form.save()
+            return redirect("welcome")
+    else:
+        form = MeetingForm()
+    return render(request, "meetings/new.html", 
+                  {"form": form})
